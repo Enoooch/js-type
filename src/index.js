9e97f49d10563c2163655f82d49f589a732646d1
Enoooch <tjc.enoch.c@gmail.com>
2021-07-19 17:14:09 +0800

    Add description

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ for (var i = 0, type; type = types[i++];) {
   })(type)
 };
 
+
+/**
+ *  @description Number Section
+ */
+
 is.infinite = function (value) {
   return value === Infinity || value === -Infinity;
 };
@@ -27,6 +32,11 @@ is.odd = function (value) {
   return is.infinite(value) || (is.number(value) && value === value && value % 2 !== 0);
 };
 
+
+/**
+ *  @description Number Section
+ */
+
 is['null'] = is['nil'] = function (value) {
   return value === null;
 };
@@ -39,6 +49,11 @@ is.defined = function (value) {
   return typeof value !== 'undefined';
 };
 
+
+/**
+ *  @description Boolean Section
+ */
+
 is['false'] = function (value) {
   return is.bool(value) && value === false;
 };
@@ -46,6 +61,11 @@ is['false'] = function (value) {
 is['true'] = function (value) {
   return is.bool(value) && value === true;
 };
+
+
+/**
+ *  @description Function Section
+ */
 
 is.normalFunction = function (value) {
   return toStr.call(value) === '[object Function]';
@@ -62,6 +82,11 @@ is.asyncFunction = function (value) {
 is['function'] = is.fn = function (value) {
   return is.normalFunction(value) || is.generatorFunction(value) || is.asyncFunction(value);
 };
+
+
+/**
+ *  @description add semantic function
+ */
 
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
